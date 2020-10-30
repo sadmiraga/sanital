4c2fdeb2c6c7c06623f25bf1b89b0f28d1cc3cba
sadmiraga <sadmirvela@gmail.com>
2020-10-31 00:08:07 +0100

    design init

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,4 +25,9 @@ class HomeController extends Controller
     {
         return view('home');
     }
+
+    public function design()
+    {
+        return view('layouts.mainLayout');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
+Route::get('/home', 'HomeController@index')->name("home");
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/index', 'HomeController@design');
