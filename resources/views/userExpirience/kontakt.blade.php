8ecfb945e97e999abcc7e3a9c3906074830c6f26
sadmiraga <sadmirvela@gmail.com>
2020-11-03 21:30:55 +0100

    kontakt Fix

--- resources/views/userExpirience/kontakt.blade.php
+++ resources/views/userExpirience/kontakt.blade.php
@@ -21,8 +21,11 @@
         <p class="smallText"> 07:30-12:00 und 13:00 bis 17:00 Uhr </p>
     </div>
 
+    <div class="mapContainer">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2698.9524165837765!2d8.559069015393463!3d47.43237117917342!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479aa00824d57cf3%3A0x9520f270bb5d690d!2sEuropa-Strasse%2017%2C%208152%20Opfikon%2C%20%C5%A0vicarska!5e0!3m2!1shr!2ssi!4v1604202795838!5m2!1shr!2ssi" width="100%" height="600" frameborder="0"  allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+    </div>
 
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2698.9524165837765!2d8.559069015393463!3d47.43237117917342!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479aa00824d57cf3%3A0x9520f270bb5d690d!2sEuropa-Strasse%2017%2C%208152%20Opfikon%2C%20%C5%A0vicarska!5e0!3m2!1shr!2ssi!4v1604202795838!5m2!1shr!2ssi" width="800" height="600" frameborder="0" style="border:0;margin-bottom:3%;margin-top:3%;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+
 
 </div>
 @endsection
