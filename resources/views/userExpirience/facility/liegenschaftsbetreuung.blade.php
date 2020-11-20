4c666fcb034ae99df3dd4a481b8161ada0644180
sadmiraga <sadmirvela@gmail.com>
2020-11-20 19:31:04 +0100

    moja mati

--- resources/views/userExpirience/facility/liegenschaftsbetreuung.blade.php
+++ resources/views/userExpirience/facility/liegenschaftsbetreuung.blade.php
@@ -1,6 +1,22 @@
 @extends('layouts.facilityLayout')
 
 @section('content')
+<div class="facility-hero">
+    <img src="/images/blueFacility.png" alt="img">
+    </div>
+    <div class="hero-text-box-facility">
+        <h1>Liegenschaftsbetreuung</h1>
+    </div>
+    <div class="facility-content">
+        <div class="facility-h">
+            <h1>Die Firma Sanical AG</h1>
+            <h4>besitzt ein grosses Wissen an speziellen Reinigungsverfahren und wendet diese
+            praxisorientiert an. Putzen kann jeder, aber Reinigung ist etwas ganz
+            anderes. Die Branche Facility Management beinhaltet viele Untergruppierungen die wir Ihnen gerne einzeln erklären und
+            gerne anbieten:</h4>
+
+        </div>
 
 
+</div>
 @endsection
