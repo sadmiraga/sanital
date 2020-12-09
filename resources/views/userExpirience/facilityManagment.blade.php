72306c1ad251c20c37a470ebcd73ef6e0c2a9ed4
Contraboi <69647487+Contraboi@users.noreply.github.com>
2020-12-09 17:23:48 +0100

    bolek

--- resources/views/userExpirience/facilityManagment.blade.php
+++ resources/views/userExpirience/facilityManagment.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.facilityLayout')
 
 @section('content')
-    <div class="facility-hero">
-        <img src="/images/blueFacility.png" alt="img">
+    <div class="hero-image">
+        <div class="hero-text-box-facility">
+            <h1>Facility Management</h1>
+        </div>
     </div>
-    <div class="hero-text-box-facility">
-        <h1>Facility Management</h1>
-    </div>
+
     <div class="facility-content">
         <div class="facility-h">
             <h1>Die Firma Sanical AG,</h1>
@@ -17,119 +17,119 @@
                 gerne anbieten:</h4>
 
         </div>
-
-        <div class="container">
-            <div class="row row1">
-                <div class="col-sm">
-                    <div class="facility-img">
-                        <img src="/images/baureinigung.jpg" alt="">
-                    </div>
-                </div>
-                <div class="col-sm">
-                    <div class="facility-textbx">
-                        <h2>Baureinigung: </h2>
-                        <p>
-                            Die Baustellen nehmen heutzutage kein Ende, doch für einen sauberen Ablauf und eine
-                            strukturierte Übersicht,
-                            sind hier Fachleute gefragt. Während der Bauphase können Unternehmen nicht
-                            Zeit vergeuden, indem Sie noch die Baustelle sauber halten. Gebäudereiniger/Baureinigung
-                            übernehmen hier die
-                            Verantwortung und führen die Grobreinigung aus in Absprache mit der
-                            Bauleitung. Sie bestellen auf Wunsch auch Mulden und halten die Baustelle ordentlich bis die
-                            Endphase
-                            eintritt. Sobald das Objekt sich dem Ende nähert, wird es für die Übergabe vorbereitet.
-                            Die Gebäudereiniger reinigen mal alles komplett durch um ein Gesamtbild zu schaffen. Sobald die
-                            Nacharbeiten abgeschlossen sind tritt die
-                            Schlussreinigung ein und das Objekt wird abgenommen
-                        </p>
-                    </div>
+    </div>
+    <div class="container">
+        <div class="row row1">
+            <div class="col-sm">
+                <div class="facility-img">
+                    <img src="/images/baureinigung.jpg" alt="">
                 </div>
             </div>
-
-            <div class="row">
-                <div class="col-sm">
-                    <h2>Spezialreinigung: </h2>
+            <div class="col-sm">
+                <div class="facility-textbx">
+                    <h2>Baureinigung: </h2>
                     <p>
-                        Chemie und verschiedene Prozesse treffen dass auf der Oberfläche und dies wird bearbeitet. Nicht zu
-                        unterschätzen,
-                        dass viele spezielle Steine oder teure Fassadenelemente gerettet wurden, durch die
-                        richtige Behandlung. Je nach Material wird ein anderes Produkt verwendet mit den geeigneten PH-Wert
-                        um die
-                        Oberflächenverschmutzung bis in die Poren zu entfernen. Reinigungsverfahren und
-                        Voraussetzungen wie: Temperatur, Produkt, Zeit und Verfahren müssen hierbei sehr gut beachtet
-                        werden. Auch
-                        mit ganz gefährlichen Stoffen können wir hantieren um z.B Glasskratzer zu
-                        entfernen
-                    </p>
-                </div>
-                <div class="col-sm">
-                    <div class="facility-img">
-                        <img src="/images/special.jpg" alt="Spezialreinigung">
-                    </div>
-                </div>
-            </div>
-
-            <div class="row row2">
-                <div class="col-sm">
-                    <div class="facility-img">
-                        <img src="/images/fassade.jpg" alt="Fassadenreinigung">
-                    </div>
-                </div>
-                <div class="col-sm">
-
-                    <h2>Fassadenreinigung: </h2>
-                    <p>
-                        Unwetter und Regen wie auch andere Einflüsse, lassen die Fassade trotz allem mit der Zeit alt
-                        aussehen.
-                        Die Nachhaltigkeit wird durch Pflege gewährleistet, deswegen müssen auch Fassaden
-                        aller Art
-                        gereinigt werden. Ob es eine Glassfassade ist oder Metall wie auch Stein, wir können alles reinigen
-                        und
-                        dies auch mit den dazugehörigen Mitte ln
+                        Die Baustellen nehmen heutzutage kein Ende, doch für einen sauberen Ablauf und eine
+                        strukturierte Übersicht,
+                        sind hier Fachleute gefragt. Während der Bauphase können Unternehmen nicht
+                        Zeit vergeuden, indem Sie noch die Baustelle sauber halten. Gebäudereiniger/Baureinigung
+                        übernehmen hier die
+                        Verantwortung und führen die Grobreinigung aus in Absprache mit der
+                        Bauleitung. Sie bestellen auf Wunsch auch Mulden und halten die Baustelle ordentlich bis die
+                        Endphase
+                        eintritt. Sobald das Objekt sich dem Ende nähert, wird es für die Übergabe vorbereitet.
+                        Die Gebäudereiniger reinigen mal alles komplett durch um ein Gesamtbild zu schaffen. Sobald die
+                        Nacharbeiten abgeschlossen sind tritt die
+                        Schlussreinigung ein und das Objekt wird abgenommen
                     </p>
                 </div>
             </div>
-
-            <div class="row">
-                <div class="col-sm">
-                    <h2>Unterhaltsreinigung: </h2>
-                    <p>Die tägliche oder auch wöchentliche Büroreinigung, wie andere Räumlichkeiten sind uns kein Stein im
-                        Weg.
-                        Gebäudereiniger im Bereich Unterhalt übernehmen die Aufgabe mit neutralem
-                        Duftreiniger Feinstaub zu entfernen und dies hygienisch zu tun. Nach Absprache mit dem Kunden werden
-                        Ihre
-                        Bedürfnisse im Bereich Reinigung wahrgenommen und so umgesetzt, dass es
-                        den ethischen und hygienischen Standarten entspricht.
-                    </p>
-                </div>
-                <div class="col-sm">
-                    <div class="facility-img">
-                        <img src="/images/unterhalt.jpg" alt="Unterhaltsreinigung">
-                    </div>
-                </div>
-            </div>
-
-            <div class="row row3">
-                <div class="col-sm">
-                    <div class="facility-img">
-                        <img src="/images/liegschaft.jpg" alt="Liegenschaftsbetreuung">
-                    </div>
-                </div>
-                <div class="col-sm">
-                    <h2>Liegenschaftsbetreuung: </h2>
-                    <p>
-                        Umgebung, Lifte wie auch im Gebäude die Treppenhäuser und Gebäudezugänge müssen gereinigt werden. Es
-                        ist
-                        nicht nur die Hygiene die hier eine grosse Rolle spielt, sondern auch die
-                        Sicherheit der Flucht-Wege ist hier auch gefragt. Ein Ansprechpartner wird diese Aufgabe übernehmen
-                        und dies
-                        für Sie nach den Vorgaben erfüllen.
-                        <br> <a href="/liegenschaftsbetreuung">Mehr sehen</a>
-                    </p>
-                </div>
-            </div>
-
-
         </div>
+
+        <div class="row">
+            <div class="col-sm">
+                <h2>Spezialreinigung: </h2>
+                <p>
+                    Chemie und verschiedene Prozesse treffen dass auf der Oberfläche und dies wird bearbeitet. Nicht zu
+                    unterschätzen,
+                    dass viele spezielle Steine oder teure Fassadenelemente gerettet wurden, durch die
+                    richtige Behandlung. Je nach Material wird ein anderes Produkt verwendet mit den geeigneten PH-Wert
+                    um die
+                    Oberflächenverschmutzung bis in die Poren zu entfernen. Reinigungsverfahren und
+                    Voraussetzungen wie: Temperatur, Produkt, Zeit und Verfahren müssen hierbei sehr gut beachtet
+                    werden. Auch
+                    mit ganz gefährlichen Stoffen können wir hantieren um z.B Glasskratzer zu
+                    entfernen
+                </p>
+            </div>
+            <div class="col-sm">
+                <div class="facility-img">
+                    <img src="/images/special.jpg" alt="Spezialreinigung">
+                </div>
+            </div>
+        </div>
+
+        <div class="row row2">
+            <div class="col-sm">
+                <div class="facility-img">
+                    <img src="/images/fassade.jpg" alt="Fassadenreinigung">
+                </div>
+            </div>
+            <div class="col-sm">
+
+                <h2>Fassadenreinigung: </h2>
+                <p>
+                    Unwetter und Regen wie auch andere Einflüsse, lassen die Fassade trotz allem mit der Zeit alt
+                    aussehen.
+                    Die Nachhaltigkeit wird durch Pflege gewährleistet, deswegen müssen auch Fassaden
+                    aller Art
+                    gereinigt werden. Ob es eine Glassfassade ist oder Metall wie auch Stein, wir können alles reinigen
+                    und
+                    dies auch mit den dazugehörigen Mitte ln
+                </p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm">
+                <h2>Unterhaltsreinigung: </h2>
+                <p>Die tägliche oder auch wöchentliche Büroreinigung, wie andere Räumlichkeiten sind uns kein Stein im
+                    Weg.
+                    Gebäudereiniger im Bereich Unterhalt übernehmen die Aufgabe mit neutralem
+                    Duftreiniger Feinstaub zu entfernen und dies hygienisch zu tun. Nach Absprache mit dem Kunden werden
+                    Ihre
+                    Bedürfnisse im Bereich Reinigung wahrgenommen und so umgesetzt, dass es
+                    den ethischen und hygienischen Standarten entspricht.
+                </p>
+            </div>
+            <div class="col-sm">
+                <div class="facility-img">
+                    <img src="/images/unterhalt.jpg" alt="Unterhaltsreinigung">
+                </div>
+            </div>
+        </div>
+
+        <div class="row row3">
+            <div class="col-sm">
+                <div class="facility-img">
+                    <img src="/images/liegschaft.jpg" alt="Liegenschaftsbetreuung">
+                </div>
+            </div>
+            <div class="col-sm">
+                <h2>Liegenschaftsbetreuung: </h2>
+                <p>
+                    Umgebung, Lifte wie auch im Gebäude die Treppenhäuser und Gebäudezugänge müssen gereinigt werden. Es
+                    ist
+                    nicht nur die Hygiene die hier eine grosse Rolle spielt, sondern auch die
+                    Sicherheit der Flucht-Wege ist hier auch gefragt. Ein Ansprechpartner wird diese Aufgabe übernehmen
+                    und dies
+                    für Sie nach den Vorgaben erfüllen.
+                    <br> <a href="/liegenschaftsbetreuung">Mehr sehen</a>
+                </p>
+            </div>
+        </div>
+
+
+    </div>
     </div>
 @endsection
